ae6ae10cc543dc04131fc97b7fc406f5ca390231
Castropy <randrescastroi@gmail.com>
2026-01-02 20:16:15 -0400

    Correcion en nombres e imports.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LogginComponent } from './loggin/loggin.component';
+  // 👈 importa tu componente
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, LogginComponent],  // 👈 agrégalo aquí
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('mi-primer-proyecto');
---
+++ src/app/loggin/loggin.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loggin',
+  standalone: true,
+  templateUrl: './loggin.component.html',
+  styleUrls: ['./loggin.component.css']
+})
+export class LogginComponent {}
